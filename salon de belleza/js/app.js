e94f523a4dc7fdd1f31f1a39fa815e0e9bc696ba
Alejandro Olarte <olartealejandro48@gmail.com>
2023-02-14 14:46:16 -0500

    creer el proyecto de salon de belleza y real state

---
+++ salon de belleza/js/app.js
@@ -0,0 +1,19 @@
+
+
+
+  var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      effect: "fade",
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay:{
+        delay:2000,
+      },
+      loop:true,
+
+    });
+
+
+ 
